825ff91351c15d7f4f1c0087aff1ca06d1223289
Sanket Dedhia <sanketdedhia97@gmail.com>
2020-04-23 11:41:55 -0500

    First Commit

---
+++ src/build/precache-manifest.2a227a374f31c1bb55892e39a2306846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c7a0473a66a9374a32c0ecf46e8225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ec2a70ed0877b08065",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "03d6dfdb49e1f0428b3f",
+    "url": "/static/js/2.1fc4d83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fc4d83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ec2a70ed0877b08065",
+    "url": "/static/js/main.e95627c2.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
